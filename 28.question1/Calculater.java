bfbf2fba7c25f5e49791b4f21b71adfdd643bcd6
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-28 14:21:45 +0530

    Simple Calculator using operators only

---
+++ 28.question1/Calculater.java
@@ -0,0 +1,18 @@
+class Calculater
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		int d=b-a;
+		int e=a*b;
+		int f=b/2;
+		int g=b%2;
+		System.out.println("The addition of 10 and 20 is "+c);
+		System.out.println("The subtraction of 20 and 10 is "+d);
+		System.out.println("The multiplication of 10 and 20 is "+e);
+		System.out.println("The quotion of 20 divided by 2 is "+f);
+		System.out.println("The modulus of 20 dived by 2 is "+g);
+	}
+}  
