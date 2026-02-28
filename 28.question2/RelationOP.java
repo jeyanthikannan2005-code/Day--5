30b0dae567ece9b51b39ea2583c1930cfa094e40
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-28 14:22:56 +0530

    compare two integers using relational operators

---
+++ 28.question2/RelationOP.java
@@ -0,0 +1,31 @@
+class RelationOP
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		Boolean equal= a==b;
+		Boolean notequal= a!=b;
+		Boolean greater= a>b;
+		Boolean lesser= a<b;
+		Boolean greaterequal= a>=b;
+		Boolean lessequal= a<=b;
+		System.out.println("a=10 and b=20 ");
+		System.out.println("a is equal to b "+equal);
+		System.out.println("a is not equal to b "+notequal);
+		System.out.println("a is greater than b "+greater);
+		System.out.println("a is lesser than b "+lesser);
+		System.out.println("a is greater than or equal to b "+greaterequal);
+		System.out.println("a is lesser than or equal to b "+lessequal);
+	}
+}
+
+
+
+
+
+
+
+
+
+				
